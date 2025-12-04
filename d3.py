6ba7f5e4f5f74c77dbdc9ee4354904521273b81d
Oscar Börjesson <oscar.boerjesson@gmail.com>
2025-12-04 09:52:05 +0100

    d3p2 complete

--- d3.py
+++ d3.py
@@ -12,3 +12,32 @@ for line in lines:
                 largestNum = int(num+num2)
     largestNums += largestNum
 print("p1: " + str(largestNums))
+
+#p2
+
+def lineMath(line):
+    # make a new list of lists, populate with shorter lines
+    newLines = []
+    # remove 1 char from line for each char in line
+    for i in range(len(line)):
+        newLine = line[:i] + line[i+1:]
+        newLines.append(newLine)
+    # convert each row in newLine to int for comparison, then sort and grab highest value
+    newLinesInt = []
+    for newLineInt in newLines:
+        newLineInt = int(newLineInt)
+        newLinesInt.append(newLineInt)
+    newLinesInt.sort()
+    largestNum = newLinesInt[-1]
+    newLine = str(newLinesInt[-1])
+    return newLine, largestNum
+
+largestNums = 0
+for line in lines:
+    line = line.rstrip()
+    while len(line) > 12:
+        newLine, largestNum = lineMath(line)
+        line = newLine
+    largestNums += int(largestNum)
+print(largestNums)
+    
